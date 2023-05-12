7e308f9d4ab6531f0129827e479101c1e58f51db
kostrominoff <zootikchannel@gmail.com>
2023-05-12 19:59:25 +0500

    Hotfix and visual

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -4,9 +4,9 @@ vim.cmd.colorscheme("catppuccin-macchiato")
 vim.cmd("TransparentEnable")
 
 require("catppuccin").setup({
-  transparent_background = true
+	transparent_background = true,
 })
 
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#5b6078' })
-vim.api.nvim_set_hl(0, 'LineNr', { fg = '#cad3f5' })
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#5b6078' })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#5b6078" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#cad3f5" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#5b6078" })
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,4 +1,5 @@
 require("neo-tree").setup({
+	popup_border_style = "rounded",
 	filesystem = {
 		hijack_netrw_behavior = "open_current",
 	},
