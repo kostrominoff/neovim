82a2896138b3a4e852bc84bfdce70e823826914a
kostrominoff <zootikchannel@gmail.com>
2023-05-21 15:28:42 +0500

    Disable formatting typescript

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,4 +1,10 @@
-require("typescript").setup({})
+require("typescript").setup({
+  server = {
+    on_attach = function(client)
+        client.resolved_capabilities.document_formatting = false
+    end,
+  }
+})
 
 local Format = vim.api.nvim_create_augroup("Format", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
